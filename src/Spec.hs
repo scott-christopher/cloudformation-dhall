24ba597d7cc4c77d1bfba59e6f31d9cd7b5948b8
Scott Christopher <schristopher@konputa.com>
2019-02-24 11:26:25 +1100

    Handle inconsistencies in the spec and code tidy-up

--- src/Spec.hs
+++ src/Spec.hs
@@ -3,65 +3,77 @@
 
 module Spec where
 
-import Control.Applicative
-import Data.Aeson
-import Data.Aeson.Types
-import Data.Foldable
-import Data.Map
-import Data.Maybe
-import Data.List as L
-import Data.Text as T
-import GHC.Generics
+import Control.Applicative  ( (<|>) )
+import Data.Aeson           ( FromJSON
+                            , Object
+                            , parseJSON
+                            , withObject
+                            , withText
+                            , (.:)
+                            , (.:?)
+                            , (.!=)
+                            )
+import Data.Aeson.Types     ( Parser )
+import Data.Foldable        ( asum ) 
+import Data.Map             ( Map, empty, singleton )
+import Data.Text            ( Text )
 
-prefixedField :: String -> String -> String
-prefixedField field = fromJust . L.stripPrefix field
-
-prefixedOptions :: String -> Options
-prefixedOptions prefix = defaultOptions
-  { fieldLabelModifier = prefixedField prefix
-  }
+-----------------------------------------------
 
 data Spec = Spec
   { specPropertyTypes :: Map Text PropertyTypes
   , specResourceTypes :: Map Text ResourceSpec
   , specResourceSpecificationVersion :: Text
-  } deriving (Generic, Show)
-
-instance FromJSON Spec where
-  parseJSON = genericParseJSON $ prefixedOptions "spec"
+  } deriving (Show)
 
 data PropertyTypes = PropertyTypes
-  { propTypeDocumentation :: Maybe Text
-  , propTypeProperties    :: Maybe (Map Text PropertySpec)
-  } deriving (Generic, Show)
+  { propTypeDocumentation :: Text
+  , propTypeProperties    :: Map Text PropertySpec
+  } deriving (Show)
 
-instance FromJSON PropertyTypes where
-  parseJSON = genericParseJSON $ prefixedOptions "propType"
+data IsRequired = Required | Optional | RequiredUndefined
+  deriving (Show)
 
 data PropertySpec = PropertySpec
   { propDocumentation :: Text
-  , propRequired      :: Bool
+  , propRequired      :: IsRequired
   , propUpdateType    :: UpdateType
   , propType          :: PropertyType
-  } deriving (Generic, Show)
+  } deriving (Show)
 
-instance FromJSON PropertySpec where
-  parseJSON = withObject "PropertySpec" $ \v -> PropertySpec
-      <$> v .: "Documentation"
-      <*> v .: "Required"
-      <*> v .: "UpdateType"
-      <*> parseType v
+data PropertyType = ListProp Bool PropertyType
+                  | MapProp PropertyType
+                  | ResourceProp Text
+                  | PrimProp PrimType
+  deriving (Show)
+
+data PrimType = PString | PLong | PInteger | PDouble | PBoolean | PTimestamp | PJson
+  deriving (Show)
+
+data UpdateType = Mutable | Immutable | Conditional | UpdateTypeUndefined
+  deriving (Show)
+
+data ResourceSpec = ResourceSpec
+  { resourceAttributes :: Map Text PropertyType
+  , resourceDocumentation :: Text
+  , resourceProperties :: Map Text PropertySpec
+  } deriving (Show)
+
+-----------------------------------------------
+
+isRequired :: Bool -> IsRequired
+isRequired True  = Required
+isRequired False = Optional
 
 parseType :: Object -> Parser PropertyType
 parseType v = asum [ v .: "Type" >>= parsePropType v
                    , PrimProp <$> v .: "PrimitiveType" 
                    , v .: "PrimitiveType" >>= parseMapPrim
                    ]
-
--- workaround for "PrimitiveType": "Map" found in AWS::ServiceDiscovery::Instance.Properties.InstanceAttributes
-parseMapPrim :: Text -> Parser PropertyType
-parseMapPrim "Map" = pure $ MapProp (PrimProp PString)
-parseMapPrim _     = fail "Not a Primitive Map"
+                where
+                  parseMapPrim :: Text -> Parser PropertyType -- workaround for "PrimitiveType": "Map"
+                  parseMapPrim "Map" = pure $ MapProp (PrimProp PString)
+                  parseMapPrim _     = fail "Not a Primitive Map"
 
 parsePropType :: Object -> Text -> Parser PropertyType
 parsePropType v "List" = ListProp <$> v .:? "DuplicatesAllowed" .!= False <*> itemType v 
@@ -72,42 +84,55 @@ itemType :: Object -> Parser PropertyType
 itemType v = ResourceProp <$> v .: "ItemType"
          <|> PrimProp <$> v .: "PrimitiveItemType"
 
-data PropertyType = ListProp Bool PropertyType
-                  | MapProp PropertyType
-                  | ResourceProp Text
-                  | PrimProp PrimType
-  deriving (Generic, Show)
+-----------------------------------------------
+
+instance FromJSON Spec where
+  parseJSON = withObject "Spec" $ \v -> Spec
+    <$> v .: "PropertyTypes"
+    <*> v .: "ResourceTypes"
+    <*> v .: "ResourceSpecificationVersion"
+
+instance FromJSON PropertyTypes where
+  parseJSON = withObject "PropertyTypes" $ \v -> asum
+    [ PropertyTypes <$> v .: "Documentation" <*> v .: "Properties"
+    , PropertyTypes "" <$> -- Workaround for AWS::EC2::LaunchTemplate.CapacityReservationPreference
+        (singleton "" . PropertySpec "" RequiredUndefined UpdateTypeUndefined <$> parseType v)
+    ]
 
 instance FromJSON PropertyType where
   parseJSON = withObject "PropertyType" parseType
 
-data PrimType = PString | PLong | PInteger | PDouble | PBoolean | PTimestamp | PJson
-  deriving (Generic, Show)
-
-primTypeFromText :: Text -> Parser PrimType
-primTypeFromText "String"    = pure PString
-primTypeFromText "Long"      = pure PLong
-primTypeFromText "Integer"   = pure PInteger
-primTypeFromText "Double"    = pure PDouble
-primTypeFromText "Boolean"   = pure PBoolean
-primTypeFromText "Timestamp" = pure PTimestamp
-primTypeFromText "Json"      = pure PJson
-primTypeFromText _           = fail "invalid PrimitiveType"
-
 instance FromJSON PrimType where
   parseJSON = withText "PrimitiveType" primTypeFromText
+    where
+      primTypeFromText :: Text -> Parser PrimType
+      primTypeFromText "String"    = pure PString
+      primTypeFromText "Long"      = pure PLong
+      primTypeFromText "Integer"   = pure PInteger
+      primTypeFromText "Double"    = pure PDouble
+      primTypeFromText "Boolean"   = pure PBoolean
+      primTypeFromText "Timestamp" = pure PTimestamp
+      primTypeFromText "Json"      = pure PJson
+      primTypeFromText _           = fail "invalid PrimitiveType"
 
-data UpdateType = Mutable | Immutable | Conditional
-  deriving (Generic, Show)
+instance FromJSON UpdateType where
+  parseJSON = withText "UpdateType" (pure . parseUpdateType)
+    where
+      parseUpdateType "Mutable"     = Mutable
+      parseUpdateType "Immutable"   = Immutable
+      parseUpdateType "Conditional" = Conditional
+      parseUpdateType _             = UpdateTypeUndefined
 
-instance FromJSON UpdateType
-
-data ResourceSpec = ResourceSpec
-  { resourceAttributes :: Maybe (Map Text PropertyType)
-  , resourceDocumentation :: Text
-  , resourceProperties :: Map Text PropertySpec
-  } deriving (Generic, Show)
+instance FromJSON PropertySpec where
+  parseJSON = withObject "PropertySpec" $ \v -> PropertySpec
+      <$> v .: "Documentation"
+      <*> (isRequired <$> v .: "Required")
+      <*> v .: "UpdateType"
+      <*> parseType v
 
 instance FromJSON ResourceSpec where
-  parseJSON = genericParseJSON $ prefixedOptions "resource"
+  parseJSON = withObject "ResourceSpec" $ \v -> ResourceSpec
+    <$> v .:? "Attributes" .!= Data.Map.empty
+    <*> v .: "Documentation"
+    <*> v .: "Properties"
 
